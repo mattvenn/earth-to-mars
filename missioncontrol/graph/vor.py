0cf7462d8e751b5fbea7ec6fb006a9dedfe8dfa9
matt venn <matt@mattvenn.net>
2015-11-06 11:10:35 +0000

    exp with regions

--- missioncontrol/graph/vor.py
+++ missioncontrol/graph/vor.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
+import random
 
 # make up data points
-points = np.random.rand(15,2)
+data = []
+for i in range(5):
+    d = { 'xy':  (random.random(), random.random()),
+        'c': random.random(),
+        }
+    data.append(d)
+points = [d['xy'] for d in data]
 
 # compute Voronoi tesselation
 vor = Voronoi(points)
@@ -16,6 +23,8 @@ for region in vor.regions:
     if not -1 in region:
         polygon = [vor.vertices[i] for i in region]
         if len(polygon):
-            plt.fill(*zip(*polygon), color="green")
+#            point_index = vor.point_region[region]
+            plt.fill(*zip(*polygon), color=(0.5,0.5,0.5))
 
+import ipdb; ipdb.set_trace()
 plt.show()
