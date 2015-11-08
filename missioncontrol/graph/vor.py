fa111078fc04e57c2a9c0af3b00638def9dd139d
matt venn <matt@mattvenn.net>
2015-11-08 12:04:23 +0000

    bug fix with double use of variable

--- missioncontrol/graph/vor.py
+++ missioncontrol/graph/vor.py
@@ -19,24 +19,32 @@ with open('data.pkl' ) as fh:
 
 points = [d['xy'] for d in data]
 
-# compute Voronoi tesselation
-vor = Voronoi(points)
+for p_num in range(3,len(points)):
+    print("frame %03d of %d" % (p_num, len(points)))
+    # compute Voronoi tesselation
+    vor = Voronoi(points[0:p_num])
 
+    #fig = plt.figure()
+    fig = voronoi_plot_2d(vor)
 
-# plot
-#voronoi_plot_2d(vor)
-plt.xlim(vor.min_bound[0] - 0.1, vor.max_bound[0] + 0.1)
-plt.ylim(vor.min_bound[1] - 0.1, vor.max_bound[1] + 0.1)
+    # limits
+    plt.xlim(0,640)
+    plt.ylim(0,400)
 
-# colorize
-for region, num_reg in zip(vor.regions, range(len(vor.regions))):
-    if not -1 in region:
-        polygon = [vor.vertices[i] for i in region]
-        if len(polygon):
-#            point_index = vor.point_region[region]
-            pr = list(vor.point_region)
-            p = pr.index(num_reg)
-            color = data[p]['c']
-            plt.fill(*zip(*polygon), color=color)
+    # colorize
+    for region, num_reg in zip(vor.regions, range(len(vor.regions))):
+        if not -1 in region:
+            polygon = [vor.vertices[i] for i in region]
+            if len(polygon):
+                pr = list(vor.point_region)
+                p = pr.index(num_reg)
+                color = data[p]['c']
+                plt.fill(*zip(*polygon), color=color)
 
-plt.show()
+    #plt.show()
+    fig.savefig("frames/%04d.png" % p_num)
+    plt.close(fig)
+
+"""
+mencoder mf://*.png -mf w=640:h=400:fps=5:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi
+"""
