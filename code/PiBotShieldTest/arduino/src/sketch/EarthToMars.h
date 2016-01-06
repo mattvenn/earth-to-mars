f36f5c1a40e9f747cc1ddc11e0004f4b2aa15aa4
matt venn <matt@mattvenn.net>
2016-01-06 14:22:21 +0000

    rfid and other peoples updates

---
+++ code/PiBotShieldTest/arduino/src/sketch/EarthToMars.h
@@ -0,0 +1,39 @@
+#ifndef ETM_h
+#define ETM_h
+
+#include <SoftwareSerial.h>
+
+/*
+pin 1 of megaspi header is cloests to rst button
+pin 1 = gnd
+pin 3 = rx
+pin 5 = +5v
+*/
+
+const byte rxPin = 11; 
+const byte txPin = 12;
+
+// set up a new serial object
+SoftwareSerial rfid_serial (rxPin, txPin);
+
+void setupETM() {
+    // put your setup code here, to run once:
+    rfid_serial.begin(9600);
+}
+
+String read_rfid()
+{
+	String rfid = "";
+	if(rfid_serial.available() == 14)
+	{
+		//discard start bit
+		rfid_serial.read();
+		for( int i = 0; i < 12; i ++ )
+			rfid += (char)rfid_serial.read();
+		//discard end bit
+		rfid_serial.read();
+	}
+	return rfid;
+}
+
+#endif
