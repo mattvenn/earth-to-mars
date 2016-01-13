8ac7c2cb991c06e601b7f4c4d533f1b9391d3936
matt venn <matt@mattvenn.net>
2016-01-13 12:01:16 +0000

    same as real

--- missioncontrol/mc/mc/config_test.py
+++ missioncontrol/mc/mc/config_test.py
@@ -7,12 +7,11 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 MAX_X = 30
 MAX_Y = 20
-GRAPH_WIDTH = 800
-GRAPH_HEIGHT = 400
+
 
 SAMPLE_TYPES = {
-    'methane': { 'min' : 0, 'max': 1, 'unit' : 'ppm' },
-    'oxygen' : { 'min' : 0, 'max': 20, 'unit' : 'ppm' },
-    'temperature' : { 'min' : 0, 'max': 273, 'unit' : 'K' },
-    'humidity' : { 'min' : 0, 'max': 20, 'unit' : '%' },
+    'methane': { 'min' : 0, 'max': 0.1, 'unit' : 'ppm' },
+    'oxygen' : { 'min' : 0, 'max': 0.1, 'unit' : 'ppm' },
+    'temperature' : { 'min' : 0, 'max': 200, 'unit' : 'K' },
+    'humidity' : { 'min' : 0, 'max': 100, 'unit' : '%' },
     }
