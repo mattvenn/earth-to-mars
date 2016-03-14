16cc9f3fc32e08cc15787645cc399700ab72db16
matt venn <matt@mattvenn.net>
2016-03-14 09:43:41 +0000

    fix range

--- code/pibot/ultrasound.py
+++ code/pibot/ultrasound.py
@@ -12,7 +12,14 @@ class Ultrasound():
         self.board.connect()
 
     def getDistance(self):
-        return int(self.board.sendCommand(Commands.READ_ULTRASOUND,TRIGGER_PIN,0))
+        distance = int(self.board.sendCommand(Commands.READ_ULTRASOUND,TRIGGER_PIN,0))
+        # large distances are reported as 0, so change to max distance
+        if distance == 0:
+            distance = 100
+        # limit to 100
+        elif distance > 100:
+            distance = 100
+        return distance
 
 if __name__ == '__main__':
 	ultra = Ultrasound()
